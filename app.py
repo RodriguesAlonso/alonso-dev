0fef539db493b477cc9d2e60919099f02ffd853d
Alonso <joaoalonso42@gmail.com>
2023-05-11 17:06:28 -0300

    agora sim

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-#import pymysql
+import pymysql
 from flask import Flask, render_template, flash, request, redirect
 from flask_bootstrap import Bootstrap5
-#from flaskext.mysql import MySQL
+from flaskext.mysql import MySQL
 
 
 app = Flask(__name__)
@@ -9,13 +9,13 @@ app.secret_key = "chave"
 
 bootstrap = Bootstrap5(app)
 
-'''
+
 app.config['MYSQL_DATABASE_USER'] = "mysql://root:WLGXoVnRJaIRJnYZeK9W@containers-us-west-158.railway.app:6991/railway"
 app.config['MYSQL_DATABASE_PASSWORD'] = "WLGXoVnRJaIRJnYZeK9W"
 app.config['MYSQL_DATABASE_DB'] = "railway"
 app.config['MYSQL_DATABASE_HOST'] = "containers-us-west-158.railway.app"
 
-mysql = MySQL(app)'''
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
@@ -23,7 +23,20 @@ def index():
 
 @app.route('/tag')
 def tag():
-    return render_template('tags.html')  
+    return render_template('tags.html')
+
+@app.route('/tarefas')
+def tarefas():
+    return render_template('tarefas.html')
+
+@app.route('/test', methods = ["GET"])
+def test():
+    conn = mysql.connect()
+    my_cursor = conn.cursor()
+    my_cursor.execute("SELECT * FROM todo")            
+    rows = my_cursor.fetchall()    
+    print(rows)
+    
     
 if __name__ == '__main__':
     app.run()
