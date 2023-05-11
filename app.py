6e0a2b054d492f9a4b727f37be5fe2ee06d29262
Alonso <joaoalonso42@gmail.com>
2023-05-11 16:55:57 -0300

    save me

--- app.py
+++ app.py
@@ -9,14 +9,13 @@ app.secret_key = "chave"
 
 bootstrap = Bootstrap5(app)
 
-mysql = MySQL()
 
 app.config['MYSQL_DATABASE_USER'] = "mysql://root:WLGXoVnRJaIRJnYZeK9W@containers-us-west-158.railway.app:6991/railway"
 app.config['MYSQL_DATABASE_PASSWORD'] = "WLGXoVnRJaIRJnYZeK9W"
 app.config['MYSQL_DATABASE_DB'] = "railway"
 app.config['MYSQL_DATABASE_HOST'] = "containers-us-west-158.railway.app"
 
-mysql(app)
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
@@ -25,7 +24,15 @@ def index():
 @app.route('/tag')
 def tag():
     return render_template('tags.html')
-   
+
+@app.route('/test', methods = ["GET"])
+def test():
+    conn = mysql.connect()
+    my_cursor = conn.cursor()
+    my_cursor.execute("SELECT * FROM todo")            
+    rows = my_cursor.fetchall()    
+    print(rows)
+    
     
 if __name__ == '__main__':
     app.run()
