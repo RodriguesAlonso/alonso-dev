cc2b43ef4dca0663b8bbf95b99ba8b671735e5f8
Alonso <joaoalonso42@gmail.com>
2023-06-02 17:00:51 -0300

    remake

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
-import pymysql
-from flask import Flask, render_template, flash, request, redirect
+from flask import Flask, render_template, flash, redirect
 from flask_bootstrap import Bootstrap5
-from flaskext.mysql import MySQL
+
 
 
 app = Flask(__name__)
-app.secret_key = "chave"
 
 bootstrap = Bootstrap5(app)
-
-
-app.config['MYSQL_DATABASE_USER'] = "mysql://root:WLGXoVnRJaIRJnYZeK9W@containers-us-west-158.railway.app:6991/railway"
-app.config['MYSQL_DATABASE_PASSWORD'] = "WLGXoVnRJaIRJnYZeK9W"
-app.config['MYSQL_DATABASE_DB'] = "railway"
-app.config['MYSQL_DATABASE_HOST'] = "containers-us-west-158.railway.app"
-
-mysql = MySQL(app)
 
 @app.route('/')
 def index():
